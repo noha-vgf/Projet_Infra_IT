31c61f0f0e9ff21b23a6a8ec28d1d4d7c25ee2d5
noha-vgf <nohat9177@gmail.com>
2026-01-22 08:50:31 +0100

    Modify livres table structure and remove clients table

--- schema.sql
+++ schema.sql
@@ -1,16 +1,11 @@
-DROP TABLE IF EXISTS clients;
-CREATE TABLE clients (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    nom TEXT NOT NULL,
-    prenom TEXT NOT NULL,
-    adresse TEXT NOT NULL
-);
 DROP TABLE IF EXISTS livres;
 
 CREATE TABLE livres (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     titre TEXT NOT NULL,
     auteur TEXT NOT NULL,
-    disponible BOOLEAN DEFAULT 1
+    annee_publication INTEGER,
+    isbn TEXT,
+    disponible BOOLEAN DEFAULT 1 -- 1 pour disponible, 0 pour emprunté
 );
